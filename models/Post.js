0ad9c8009f080f89d48227b7b6f55c1ccb34085b
Chris Zavala <frappejcat@gmail.com>
2023-02-08 17:37:47 -0600

    added the last two for models post.js and comment.js

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+//again pulled from 25-Ins_Literals models->driver.js made changes to var names and some changes. 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+//instead of driver it is Post
+module.exports = Post;
