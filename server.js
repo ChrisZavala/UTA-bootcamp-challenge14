15f47f8bb9493ef89e1823873d8e3c78b85fbe7d
Chris Zavala <frappejcat@gmail.com>
2023-02-08 19:58:16 -0600

    pushing some more handlebars pages up, I need to work on the routes.

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: process.env.SECRET,
-    cookie: { maxAge: 300000 
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
+    cookie: { maxAge: 300000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
