45836996d6360bc4fb03fd90e0306ca8392007de
Chris Zavala <frappejcat@gmail.com>
2023-02-09 09:24:25 -0600

    adding more js functionality for delete and such.

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,29 @@
 //loginFormHandler()
-async function loginFormHandler(event) {
+  //Event listener
+  const loginFormHandler = async (event) => {
     event.preventDefault();
+  
+    // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+  
     if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: {'Content-Type': 'application/json'}
-        });
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            let result = await response.json()
-            alert(result.message)
-        }
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
-  }
-  //Event listener
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  };
+
+  document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,31 @@
-async function signupFormHandler(event) {
+//signup()
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+
+const signupFormHandler = async (event) => {
     event.preventDefault();
-    const username = document.querySelector('#username-signup').value.trim();
+  
+    const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: {'Content-Type': 'application/json'}
-        });
-        if (response.ok) {
-            alert('Account created! Logging you in now.');
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText)
-        }
+  
+    if (name && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        alert('Account created! Logging you in now.');
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
-}
+  };
+
 //Event Listener
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document
+.querySelector('.signup-form')
+.addEventListener('submit', signupFormHandler);
